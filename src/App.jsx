2d650a0d4b49ebac3d07a121f49da00f2df94f89
hsiaofung <hsiaofung.chou@gmail.com>
2019-11-19 21:49:52 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Loading from "./Loading";
+
+function App() {
+  return (
+    <div className="App">
+      <Loading />
+    </div>
+  );
+}
+
+export default App;
